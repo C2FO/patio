284abaf4dcc44367f6902458ef9ebb0ef3619be9
Jordan Ayers <jordan.ayers@gmail.com>
2013-09-29 15:11:34 -0500

    Add tests for validator's 'onlyNotNull' switch.

--- test/plugins/validatorPlugin.test.js
+++ test/plugins/validatorPlugin.test.js
@@ -1083,6 +1083,44 @@ it.describe("patio.plugins.ValidatorPlugin",function (it) {
 
     });
 
+    it.describe("#onlyNotNull", function (it) {
+
+        it.beforeAll(function () {
+            Model = patio.addModel("validator", {
+                plugins: [ValidatorPlugin]
+            });
+            Model.validate("str_req").isEq("HELLO",{ onlyDefined: true, onlyNotNull:  false });
+            Model.validate("str_opt").isEq("HELLO",{ onlyDefined: true, onlyNotNull:  true  });
+            return  Model.sync();
+        });
+
+        it.should("throw an error if required field is null", function (next) {
+            var m = new Model({str_req: null, str_opt: "HELLO"});
+            assert.isFalse(m.isValid());
+            m.save().then(next, function (err) {
+                assert.equal(err[0].message, "str_req must === HELLO got null.");
+                next();
+            });
+        });
+
+        it.should("not throw an error if valid and not null", function (next) {
+            comb.when(
+                new Model({str_req: "HELLO", str_opt: "HELLO"}).save()
+            ).classic(next);
+        });
+
+        it.should("not throw an error if valid and optional fields are null", function (next) {
+            comb.when(
+                new Model({str_req: "HELLO", str_opt: null}).save()
+            ).classic(next);
+        });
+
+        it.should("not throw an error if values are undefined", function (next) {
+            new Model().save().classic(next);
+        });
+
+    });
+
     it.afterAll(function () {
         return helper.dropModels();
     });
