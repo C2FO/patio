12335998ed854449b1c7581885e75142471f24c5
Karel Gonzalez <karel.gonzalez@c2fo.com>
2021-05-27 15:58:52 -0500

    Changed main page button text

--- ng-docs/src/pages/index.js
+++ ng-docs/src/pages/index.js
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/index">
+            Get Started!️
           </Link>
         </div>
       </div>
